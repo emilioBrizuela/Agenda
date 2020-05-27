92bec4d63e1e2250a07c56d72a9ff5c4651135e4
Emilio Brizuela <emiliobrizuela92@gmail.com>
2020-05-27 09:04:10 +0200

    Teclado y pantall

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Telefono t = new Telefono();
+        t.setSize(230,350);
+        t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        t.setVisible(true);
     }
 }
